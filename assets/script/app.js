012ab91d917dbf67d7280e20cc21b967b108e67f
Evan Kennedy <evanckennedy@protonmail.com>
2024-03-22 14:39:16 -0500

    create shape functionality

--- assets/script/app.js
+++ assets/script/app.js
@@ -7,7 +7,42 @@ import Shape from './Shape.js';
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-/*  Organizer                                            */
+/*  Selectors                                            */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+const shapeSelection = utils.select('.shape-select')
+const colorSelection = utils.select('.color-select')
+const createButton = utils.select('.selection-container input')
+const gridContainer = utils.select('.grid-container');
+const message = utils.select('.message p');
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Create Shape                                         */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
-utils.print('Welcome');
+
+function createShape(shape, color) {
+  const shapeDiv = document.createElement('div');
+  shapeDiv.classList.add('shape');
+  shapeDiv.style.backgroundColor = color;
+
+  if (shape === 'square') {
+    shapeDiv.classList.add('square');
+  }
+
+  if (shape === 'circle') {
+    shapeDiv.classList.add('circle');
+  }
+
+  gridContainer.appendChild(shapeDiv);
+}
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+/*  Event listeners                                      */
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+utils.listen('click', createButton, () => {
+  const selectedShape = shapeSelection.value;
+  const selectedColor = colorSelection.value;
+  console.log(selectedColor);
+  console.log(selectedShape);
+  createShape(selectedShape, selectedColor);
+});
